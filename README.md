64e7105406a2b96fd3716f5c1cb2bca0fae48cba
kyungseopk1m <kks0919@kakao.com>
2024-01-28 21:50:22 +0900

    :rocket: v1.1.0 npm published

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# holidays
+# holidays-kr
+
+- 공공데이터포털 내 한국천문연구원의 공휴일 정보를 기반으로 데이터를 제공합니다.
+- 2004년부터의 공휴일 정보를 제공하며, 매월 3회에 걸쳐 주기적으로 최신 데이터를 불러옵니다.
+- 11월부터 당해년도가 아닌 다음해의 공휴일 정보를 불러올 수 있습니다.
+- `commonjs`와 `ESM`을 지원합니다.
+
+## Install
+```
+npm i @kyungseopk1m/holidays-kr
+```
+
+
+## Usage
+
+```typescript
+import { holidays } from '@kyungseopk1m/holidays-kr';
+
+const result = await holidays('2024');
+
+console.log(result);    // 2024.01 ~ 2024.12 데이터
+
+// or
+
+const { holidays } = require('@kyungseopk1m/holidays-kr');
+
+const data = await holidays('2010', '2015');
+
+console.log(data);    // 2010.01 ~ 2015.12 데이터
+```
+
+## Output
+
+| 속성      | 설명 |
+|---------|-----|
+| name    | 공휴일의 이름 |
+| date    | 'YYYYMMDD' 형식의 날짜 |
+
+```typescript
+interface example {
+    "date": 20240101,
+    "name": "1월1일"
+}
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const { AXIOS_URL } = process.env;
+
+export const holidays = async (year: string, year2?: string) => {
+    if (year.length !== 4 || (year2 && year2.length !== 4)) {
+        return {
+            success: false,
+            message: 'Please enter the year correctly.',
+            data: [],
+        }
+    }
+
+    try {
+        const result = await axios.post(AXIOS_URL as string, {
+            year,
+            year2,
+        });
+        if (result.status !== 200) {
+            return {
+                success: false,
+                message: 'Failed to fetch holiday data',
+                data: [],
+            }
+        } else {
+            return {
+                success: true,
+                message: 'Success',
+                data: result.data,
+            }
+        }
+    } catch (error) {
+        return error instanceof Error ?
+            {
+                success: false,
+                message: error.message,
+                data: [],
+            } :
+            {
+                success: false,
+                message: 'Unknown error',
+                data: [],
+            }
+    }
+}
